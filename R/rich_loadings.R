3ef28cacb2eaf2532e1ad338df594e7eeb41e91d
mcalgaro93 <mcalgaro93@gmail.com>
2019-11-05 11:46:21 +0100

    Change from equal to fixed

--- R/rich_loadings.R
+++ R/rich_loadings.R
@@ -105,7 +105,7 @@ HotLoadings.heat_map_long <- function(data.splsda,top_feature,PSOBJ,sample_name,
 
   ggplot(clr_matrix_sel_melt,aes(clr_matrix_sel_melt[,Y_name],y = feature, fill = clr)) +
     geom_tile(colour = "white") +
-    coord_equal() +
+    coord_fixed() +
     theme_minimal() +
     theme(axis.text.x = element_text(angle = 45,hjust = 1,vjust = 1),
           axis.text.y = element_blank(),
